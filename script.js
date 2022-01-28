b317980a1080b075fbdd0e41ad00eebbdf6fb558
xomyak-coder <dkhol@inbox.ru>
2022-01-28 13:38:42 +0300

    Done second base homework

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "GS_GLO";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 300;
+let rollback = 4;
+let fullPrice = 500000;
+let adaptive = true;
 
-alert("Glo aademy рулит!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(
+  "Стоимость верстки экранов " +
+    screenPrice +
+    " рублей\n" +
+    "Стоимость разработки сайта " +
+    fullPrice +
+    " рублей"
+);
 
-console.log("Какой-то текст");
+let screensLower = screens.toLowerCase();
+console.log(screensLower.split());
+
+let percentToWork = fullPrice * (rollback / 100);
+console.log(percentToWork);
