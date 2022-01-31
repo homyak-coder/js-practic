4d3f7ca0a7a19b003352f0f4616a32550adfe371
xomyak-coder <dkhol@inbox.ru>
2022-01-31 13:06:35 +0300

    Done hard third homework

--- script-hard.js
+++ script-hard.js
@@ -15,3 +15,60 @@ let powStr = String(powNum);
 let cutMul = powStr.slice(0, 2);
 
 console.log(cutMul);
+
+let lang = prompt("Введите названия языка (lang, ru)  ");
+
+if (lang == "ru") {
+  console.log("Понедельник");
+  console.log("Вторник");
+  console.log("Среда");
+  console.log("Четверг");
+  console.log("Пятница");
+  console.log("Суббота");
+  console.log("Воскресенье");
+} else {
+  console.log("Monday");
+  console.log("Tuesday");
+  console.log("Wensday");
+  console.log("Thirsday");
+  console.log("Friday");
+  console.log("Saturday");
+  console.log("Sunday");
+}
+
+switch (lang) {
+  case "ru":
+    console.log("Понедельник");
+    console.log("Вторник");
+    console.log("Среда");
+    console.log("Четверг");
+    console.log("Пятница");
+    console.log("Суббота");
+    console.log("Воскресенье");
+    break;
+  case "en":
+    console.log("Monday");
+    console.log("Tuesday");
+    console.log("Wensday");
+    console.log("Thirsday");
+    console.log("Friday");
+    console.log("Saturday");
+    console.log("Sunday");
+    break;
+}
+
+let arrayLan = [
+  [
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье",
+  ],
+  ["Monday", "Tuesday", "Wensday", "Thirsday", "Friday", "Saturday", "Sunday"],
+];
+
+alert(arrayLan[0]);
+alert(arrayLan[1]);
